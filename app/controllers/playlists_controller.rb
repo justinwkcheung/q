0d3b519cbd6af5a8e81d43299406cdbc8580e238
Richard Strauss <rstrauss91@gmail.com>
2017-02-27 16:15:34 -0500

    add create and delete playlist functionality

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,5 +2,33 @@ class PlaylistsController < ApplicationController
 
   def show
   end
-  
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def destroy
+    @playlist_q = Playlist.find(params[:id])
+    if @playlist_q.destroy
+      
+      redirect_to playlists_path
+    end
+  end
+
+  def create
+    @playlist_spotify = RSpotify::User.new(session[:user]).create_playlist!(playlist_params[:name])
+    session[:uri] = @playlist_spotify.uri
+
+    if @playlist_q = Playlist.create(name: playlist_params, uri: session[:uri])
+      redirect_to playlist_path(@playlist_q)
+
+    end
+  end
+
+private
+
+  def playlist_params
+      params.require(:playlist).permit(:name)
+  end
+
 end

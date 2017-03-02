1831d738e61cfc9e33aff9bfddfc54126cbfc063
Akshit Sahani <akshit_sahani@hotmail.com>
2017-03-02 15:17:54 -0500

    made very minor changes to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,20 @@
 class SessionsController < ApplicationController
 
-def new
-end
+  def new
+  end
 
-def create
-  @access = params[:code]
-  response = HTTParty.get("https://connect.deezer.com/oauth/access_token.php?app_id=#{ENV["deezer_application_id"]}&secret=#{ENV["deezer_secret_key"]}&code=#{params[:code]}&output=json")
+  def create
+    @access = params[:code]
+    response = HTTParty.get("https://connect.deezer.com/oauth/access_token.php?app_id=#{ENV["deezer_application_id"]}&secret=#{ENV["deezer_secret_key"]}&code=#{params[:code]}&output=json")
 
-  access_token = response["access_token"]
-  new_song = HTTParty.get("http://api.deezer.com/search?q=eminem&#{access_token}")
-  binding.pry
-end
+    access_token = response["access_token"]
+    new_song = HTTParty.get("http://api.deezer.com/search?q=eminem&#{access_token}")
+  end
 
-def destroy
-end
+  def destroy
+  end
 
-def view
-end
+  def view
+  end
 
 end

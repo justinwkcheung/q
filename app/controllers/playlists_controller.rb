4ea5238e7829984cbd4f7075f4ab2067bffea0f6
Stephen H <stephen210huang@hotmail.com>
2017-03-03 17:52:06 -0500

    player loads w/ no song

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -31,6 +31,13 @@ class PlaylistsController < ApplicationController
   def show
     @playlist_q = Playlist.find(params[:id])
     @playlist_q_songs = SuggestedSong.where(playlist_id: @playlist_q.id)
+    if !@playlist_q_songs.empty?
+      @first_song = @playlist_q_songs.first.song_id
+      @second_song = @playlist_q_songs.second.song_id
+    else
+      @first_song = 0
+      @second_song = 0
+    end
   end
 
   def new
@@ -46,14 +53,9 @@ class PlaylistsController < ApplicationController
   end
 
   def create
-
-    # if @playlist_q = Playlist.create(
-    #   name: playlist_params[:name],
-    #   description: playlist_params[:description],
-    #   theme: playlist_params[:theme],
-    #   redirect_to playlist_path(@playlist_q)
-    # end
-
+    if create_playlist
+      redirect_to playlist_path(@playlist_q)
+    end
   end
 
   def edit
@@ -76,6 +78,18 @@ private
 
   def playlist_params
       params.require(:playlist).permit(:name, :description, :theme)
+  end
+
+
+  def create_playlist
+    @playlist_q = Playlist.create(
+      name: playlist_params[:name],
+      description: playlist_params[:description],
+      theme: playlist_params[:theme])
+    @authorization = Authorization.create(
+      playlist_id: @playlist_q.id,
+      user_id: session[:user_id],
+      status: "Host")
   end
 
 end

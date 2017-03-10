915934f5cb03b33a5f85194cea0de4928aa51080
Richard Strauss <rstrauss91@gmail.com>
2017-03-10 11:10:15 -0500

    deleted extra buttons on user show page

--- app/controllers/suggestedsongs_controller.rb
+++ app/controllers/suggestedsongs_controller.rb
@@ -10,19 +10,16 @@ class SuggestedsongsController < ApplicationController
     @songs = SuggestedSong.playlist_songs(params[:playlist_id])
     @users = User.all
     ActionCable.server.broadcast(:app, @songs)
-    ActionCable.server.broadcast(:app, @users)
  end
 
   def index
     @playlist_q = Playlist.find(params[:playlist_id])
-
 
     response = HTTParty.get("https://connect.deezer.com/oauth/access_token.php?app_id=#{ENV["deezer_application_id"]}&secret=#{ENV["deezer_secret_key"]}&code=&output=json")
     access_token = response["access_token"]
     # @albums = HTTParty.get("http://api.deezer.com/search/album?q=#{params[:q]}&#{access_token}")
     @tracks = HTTParty.get("http://api.deezer.com/search/track?q=#{params[:q]}&#{access_token}")
     # @artists = HTTParty.get("http://api.deezer.com/search/artist?q=#{params[:search]}&#{access_token}")
-
 
     if request.xhr?
       respond_to do |format|

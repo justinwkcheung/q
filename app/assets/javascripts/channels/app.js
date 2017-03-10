d4d48c85a416fc17369ff3ca39cb179d58ae4074
Justin Cheung <justinwkcheung@github.com>
2017-03-09 20:06:35 -0500

    Match front-end playlist to back-end functionalities

--- app/assets/javascripts/channels/app.js
+++ app/assets/javascripts/channels/app.js
@@ -26,17 +26,17 @@ $('document').ready(function(){
             var divContainer = $('<div>').attr('class', 'song-in-queue').attr('data-playlist-id', playlist_id).attr('data-suggested-song-id', data.id);
           }
         var span = $('<span>').attr('class',"buttons")
-        var buttonUp = $('<button>').attr('type',"button").attr('name','button').attr('class','upvote btn waves-effect waves-light blue darken-2')
+        var buttonUp = $('<button>').attr('type',"button").attr('name','button').attr('class','upvote btn waves-effect waves-light blue lighten-2')
         var iconUp = $('<i>').attr('class','material-icons').html('thumb_up')
         var upButton = $(buttonUp).append(iconUp)
-        var buttonDown = $('<button>').attr('type',"button").attr('name','button').attr('class','downvote btn waves-effect waves-light red')
+        var buttonDown = $('<button>').attr('type',"button").attr('name','button').attr('class','downvote btn waves-effect waves-light red lighten-2')
         var iconDown = $('<i>').attr('class','material-icons').html('thumb_down')
         var downButton = $(buttonDown).append(iconDown)
         var spanHeart = $('<span>').attr('class','heart')
         var iconHeart = $('<i>').attr('class','fa fa-heart').attr('style','font-size:12px')
         var netVote = $('<span>').attr('class','netvote').attr('id',data.id).html(data.net_vote)
-        var heart = $(spanHeart).append(netVote).append(iconHeart)
-        var votes = $(span).append(upButton).append(downButton)
+        var heart = $(spanHeart).append(netVote).append(" ").append(iconHeart)
+        var votes = $(span).append(upButton).append(" ").append(downButton)
         var div_replace = $(divContainer).html(data.name)
         $(div_replace).append(votes).append(heart)
         $('.song-list').append(div_replace);

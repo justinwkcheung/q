a77ec74a0606b269f48f9df3e14afd2ec03dc982
Justin Cheung <justinwkcheung@github.com>
2017-03-15 20:45:06 -0400

    Attach event handler to delete guest button

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,8 +1,10 @@
 $(document).on("ready", function(){
 
   $('.modal').modal({
-    startingTop: '20%', // Starting top style attribute
-      endingTop: '10%', // Ending top style attribute
+    startingTop: '20%',
+      endingTop: '10%',
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      },
   });
 
   $('.guestlist').on('click', function(event) {
@@ -17,13 +19,21 @@ $(document).on("ready", function(){
       data.forEach(function(guest){
         var li = $('<li>').attr('class','guest');
         var span = $('<span>').html(guest[0] + ' ' + guest[1]);
-        var guestdiv = $(li).append(span);
+        var guestDelete = $('<button>').html("Remove Guest").addClass('btn-flat').addClass('guest-delete')
+        var guestdiv = $(li).append(span).append(guestDelete);
         $('.guest-list-container ol').append(guestdiv);
+        $('.guest-delete').on('click', function() {
+          console.log('deleting guest');
+        });
       })
       // $('.guest-list-container').fadeIn("slow",function(){})
     });
 
   })
+
+  // $(document).on('click', '.guest-delete', (function() { //the regular way of .guest-delete on click doesn't work but this does
+  //   console.log('deleting guest');
+  // }));
   // $('#close').on('click',function(event){
   //   $('.guest-list-container').fadeOut('fast',function(){})
   // })
